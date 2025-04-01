493c7e60356e7121a8d55fe5e4ba29251248557d
FLGZz <jhonaspimenta@gmail.com>
2025-04-01 14:15:37 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -6,11 +6,14 @@ function carregarConteudo(opcao) {
     HTMLCSS: `<h2 style="text-align: center;">Conteúdo de HTML/CSS</h2><p>Aqui está o material de HTML e CSS.</p>`,
     POOJAVA: `<h2 style="text-align: center;">Conteúdo de POO JAVA</h2><p>Aqui está o material de Programação Orientada a Objetos com Java.</p>`,
     SO: `<h2 style="text-align: center;">Conteúdo de S.O</h2><p>Aqui está o material de Sistemas Operacionais.</p><br>
+    <ul>
+      <li><button class="contbutton" onclick="textcont('contSO1')">O que é um Sistema Operacional?</button>
+    </ul>
    `,
     LOGPR: `<h2 style="text-align: center;">Conteúdo de LOGPR</h2><p>Aqui está o material de Lógica de Programação.</p>`,
     NR: `<h2 style="text-align: center;">Conteúdo de N.R</h2><p>Aqui está o material de Noções de Robótica.</p>
     <ul> 
-      <li><button class="contbutton" onclick="textNR('contNR1')"></button></li>
+      <li><button class="contbutton" onclick="textcont('contNR1')">Robótica x Automação</button></li>
     </ul>
     
     `,
@@ -33,9 +36,10 @@ function carregarConteudo(opcao) {
   const divConteudo = document.getElementById('conteudo');
   divConteudo.innerHTML = conteudo[opcao] || "<h2>Conteúdo não encontrado!</h2>";
 }
-function textNR(opcao) {
+function textcont(opcao) {
   const conteudo = {
-  contNR1: `
+    // Conteúdos de Noções de Robótica
+contNR1: `
 <h2>Qual é a diferença entre robótica e automação?</h2>
 <p>De maneira geral, a automação é um conceito abrangente que engloba softwares, máquinas e ferramentas projetadas para executar tarefas que normalmente seriam realizadas por pessoas.
 Um exemplo disso são as plataformas de automação de processos empresariais (BPA, ou business process automation em inglês), que são sistemas criados para otimizar as operações internas das organizações.
@@ -54,9 +58,30 @@ Robôs industriais são dispositivos físicos utilizados em atividades como insp
 <h2>Automação e robótica: como se relacionam?</h2>
 <p>A automação abrange uma variedade de tecnologias, incluindo programas de software sem componentes físicos, dispositivos PLC (controladores lógicos programáveis) e suas subdivisões, como a robótica, que pode ser considerada uma subcategoria dentro do campo da automação.
 </p>
-  
- `
+ `,
+    // Conteúdos de Sistemas Operacionais
+contSO1: `
+ <h3>O que é um Sistema Operacional?</h3><br>
+    <p>Um sistema operacional (SO) é o software central que faz a ponte entre o hardware do computador e os programas que você utiliza. 
+    Ele gerencia os recursos do sistema, fornece serviços essenciais e cria um ambiente onde outros programas podem rodar.</p><br>
+    <h3>Funções Básicas de um Sistema Operacional:</h3><br>
+    <ul>
+            <li>Gerenciamento de Processos: É ele quem cria, programa e encerra os processos. O SO acompanha todos os processos em execução, 
+            distribui os recursos necessários e facilita a comunicação e sincronização entre eles.</li>
+            <li>Gerenciamento de Memória: Coordena a utilização da memória do computador, definindo quais processos terão 
+            acesso a ela e por quanto tempo.</li>
+            <li>Gerenciamento de Dispositivos: Controla os dispositivos de hardware conectados, utilizando drivers específicos para intermediar a 
+            comunicação entre o software e o hardware.</li>
+            <li>Gerenciamento de Arquivos: Organiza os arquivos em dispositivos de armazenamento (como discos rígidos), 
+            definindo quem tem autorização para acessá-los e de que forma.</li>
+            <li>Segurança e Acesso: Garante que apenas usuários autorizados possam acessar os recursos do sistema, com procedimentos de login e 
+            proteção de arquivos ou diretórios.</li>
+            <li>Interface do Usuário: Oferece uma forma de interação com o sistema, seja por meio de uma interface de linha de comando (CLI) 
+            ou uma interface gráfica (GUI) com ícones e janelas.</li>
+            <li>Gerenciamento de Entrada/Saída (E/S): Coordena o funcionamento de dispositivos de E/S, como impressoras, teclados e discos.</li>
+        </ul>
 
+`
 }
 const divConteudo = document.getElementById('conteudo');
   divConteudo.innerHTML = conteudo[opcao] || "<h2>Conteúdo não encontrado!</h2>";
